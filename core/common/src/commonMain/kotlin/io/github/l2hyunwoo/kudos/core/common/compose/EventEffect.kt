c573d3e79df7e9683be34738201bcd2fa8b0df75
HyunWoo Lee <l2hyunwoo@gmail.com>
2025-10-06 23:12:13 +0900

    :sparkles: Add EventEffect, EventFlow

---
+++ core/common/src/commonMain/kotlin/io/github/l2hyunwoo/kudos/core/common/compose/EventEffect.kt
@@ -0,0 +1,34 @@
+package io.github.l2hyunwoo.kudos.core.common.compose
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.supervisorScope
+
+typealias EventFlow<T> = MutableSharedFlow<T>
+
+@Composable
+fun <T> rememberEventFlow(): EventFlow<T> {
+    return remember {
+        MutableSharedFlow(extraBufferCapacity = 20)
+    }
+}
+
+@Composable
+fun <EVENT> EventEffect(
+    eventFlow: EventFlow<EVENT>,
+    block: suspend CoroutineScope.(event: EVENT) -> Unit,
+) {
+    LaunchedEffect(eventFlow) {
+        supervisorScope {
+            eventFlow.collect { event ->
+                launch {
+                    block(event)
+                }
+            }
+        }
+    }
+}
