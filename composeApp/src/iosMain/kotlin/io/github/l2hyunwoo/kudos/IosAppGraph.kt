bc048014b2f6b98261b00392c9d9bd03ef71d96e
HyunWoo Lee <l2hyunwoo@gmail.com>
2025-10-06 15:32:32 +0900

    :bug: Fix non-authorization request issue

--- composeApp/src/iosMain/kotlin/io/github/l2hyunwoo/kudos/IosAppGraph.kt
+++ composeApp/src/iosMain/kotlin/io/github/l2hyunwoo/kudos/IosAppGraph.kt
@@ -24,10 +24,12 @@ import io.github.l2hyunwoo.kudos.core.datastore.annotation.TasksDataStore
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.darwin.Darwin
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.DEFAULT
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.request.header
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.cinterop.BetaInteropApi
 import kotlinx.cinterop.ExperimentalForeignApi
@@ -80,6 +82,9 @@ interface IosAppGraph : AppGraph {
         install(Logging) {
             logger = Logger.DEFAULT
             level = LogLevel.ALL
+        }
+        defaultRequest {
+            header("Authorization", "Bearer ${BuildConfig.SUPABASE_ANON_KEY}")
         }
     }
 
